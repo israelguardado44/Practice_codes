00886bf6958dedba8c4bd93cc531112faa1216a3
Israel <israelguardado44@yahoo.com>
2015-09-28 22:31:33 -0700

    updated files

--- Program_games_with_python_and_pygame/2015_0921_2056_r.1.3_learn_python_the_hard_way.py
+++ Program_games_with_python_and_pygame/2015_0921_2056_r.1.3_learn_python_the_hard_way.py
@@ -1,92 +1,146 @@
-import ex25
-def break_words(stuff):
-    """This function will break up words for us."""
-    words = stuff.split(' ')
-    return words
+class Scene(object):
 
-def sort_words(words):
-    """Sorts the words."""
-    return sorted(words)
+    def __init__(self):
+        pass
+        
+    def enter(self):
+        pass
 
-def print_first_word(words):
-    """Prints the first word after popping it off."""
-    word = words.pop(0)
-    print word
+class Engine(object):
 
-def print_last_word(words):
-    """Prints the last word after popping it off."""
-    word = words.pop(-1)
-    print word
+    
+    def __init__(self, current_scene):
+        self.current_scene = current_scene
+        
+    def play(self):
+        self.current_scene.enter()
 
-def sort_sentence(sentence):
-    """Takes in a full sentence and returns the sorted words."""
-    words = break_words(sentence)
-    return sort_words(words)
+        print 'Where do you want to go?'
+        print '1. Move forward'
+        print '2. Move to the right'
+        print '3. Move back'
+        print '4. Move to the left'
 
-def print_first_and_last(sentence):
-    """Prints the first and last words of the sentence."""
-    words = break_words(sentence)
-    print_first_word(words)
-    print_last_word(words)
+        direction = int(raw_input('> '))
+        while direction not in range(1,5):
+            print '\nType a number 1-4'
+            print 'Where do you want to go?'
+            print '1. Move forward'
+            print '2. Move to the right'
+            print '3. Move back'
+            print '4. Move to the left'
+            direction = int(raw_input('> '))
+        
 
-def print_first_and_last_sorted(sentence):
-    """Sorts the words then prints the first and last one."""
-    words = sort_sentence(sentence)
-    print_first_word(words)
-    print_last_word(words)
-
-
-print "Let's practice everything."
-print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
-
-poem = """
-\tThe lovely world
-with logic so firmly planted
-cannot discern \n the needs of love
-nor comprehend passion from intuition
-and requires an explantion
-\n\t\twhere there is none.
-"""
+        self.current_scene.exit(direction)
 
 
-print "--------------"
-print poem
-print "--------------"
+    def battle(self):
+        for i in range(1, len(self.current_scene.enemies)+1):
+            print str(i) + '.', self.current_scene.enemies[i-1]
+        choice = int(raw_input('Choose your opponent: '))
+        while choice not in range(1, len(self.current_scene.enemies)+1):
+            print '\nType an number from the list idiot'
+            for i in range(1, len(self.current_scene.enemies)+1):
+                print str(i) + '.', self.current_scene.enemies[i-1]
+            choice = int(raw_input('Choose your opponent: '))
+        enemy = self.current_scene.enemies[choice - 1]
 
-five = 10 - 2 + 3 - 6
-print "This should be five: %s" % five
-
-def secret_formula(started):
-    jelly_beans = started * 500
-    jars = jelly_beans / 1000
-    crates = jars / 100
-    return jelly_beans, jars, crates
-
-
-start_point = 10000
-beans, jars, crates = secret_formula(start_point)
-
-print "With a starting point of: %d" % start_point
-print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
-
-start_point = start_point / 10
-
-print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point)
+            
+        
 
 
-sentence = "All good things come to those who wait."
+class Death(Scene):
 
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
+    def enter(self):
 
-print_first_word(words)
-print_last_word(words)
-print_first_word(sorted_words)
-print_last_word(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
-print sorted_words
+        print 'Crap... you took a wrong turn an anvil fell on your head, and you died.'
+        print "You're an idiot"
 
-print_first_and_last(sentence)
+class CentralCorridor(Scene):
 
-print_first_and_last_sorted(sentence)
+    def enter(self):
+        print 'You are in the central corridor.'
+        print 'There is a room to the right'
+        print 'There is a room to straight ahead'
+
+    def exit(self, direction):
+
+        self.direction = direction
+        if self.direction == 1:
+            play_game = Engine(TheBridge())
+            play_game.play()
+        elif self.direction == 2:
+            play_game = Engine(LaserWeaponArmory())
+            play_game.play()
+            
+
+class LaserWeaponArmory(Scene):
+
+    
+    def __init__(self):
+        pass
+
+    def enter(self):
+        print 'You have entered the Weapon armory'
+        print 'The Central corridor is to the left'
+
+    def exit(self, direction):
+
+        self.direction = direction
+        if self.direction == 4:
+            play_game = Engine(CentralCorridor())
+            play_game.play()
+        else:
+            print 'Choose a different direction'
+
+class TheBridge(Scene):
+
+    enemies = ['Bob: The destroyer of worlds and the wrecker of your sh**t', 'test henchman # 5']
+    scene_items = ['The escape room key']
+
+    def __init__(self):
+        self.current_scene = 'The Bridge'
+
+    def enter(self):
+        print 'You have entered the Bridge'
+        
+        print "Enemies:"
+        for enemy in self.enemies:
+            print '\t', enemy
+
+
+class EscapePod(Scene):
+
+    def enter(self):
+        pass
+
+class Map(object):
+
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
+
+class Player(object):
+
+    def __init__(self):
+        self.life = 10
+        self.attack_dict = {'punch': 2, 'kick': 4, 'gun': 6}
+        self.item = {'stuff': 0}
+
+    def status(self):
+        print 'Life: ', self.life
+        print 'Item: ', self.item
+
+
+
+
+
+
+play_game = Engine(CentralCorridor())
+play_game.play()
